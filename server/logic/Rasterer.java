04cb77e92663da35e84edf7d42b2d75024061bc5
Jacques DeBar <35080230+jacquesdebar@users.noreply.github.com>
2020-11-01 13:48:01 -0700

    Add files via upload

---
+++ server/logic/Rasterer.java
@@ -0,0 +1,127 @@
+package huskymaps.server.logic;
+
+import huskymaps.params.RasterRequest;
+import huskymaps.params.RasterResult;
+
+import java.util.Objects;
+
+import static huskymaps.utils.Constants.MIN_X_TILE_AT_DEPTH;
+import static huskymaps.utils.Constants.MIN_Y_TILE_AT_DEPTH;
+import static huskymaps.utils.Constants.MIN_ZOOM_LEVEL;
+import static huskymaps.utils.Constants.ROOT_ULLAT;
+import static huskymaps.utils.Constants.ROOT_ULLON;
+import static huskymaps.utils.Constants.LAT_PER_TILE;
+import static huskymaps.utils.Constants.LON_PER_TILE;
+
+/** Application logic for the RasterAPIHandler. */
+public class Rasterer {
+
+    /**
+     * Takes a user query and finds the grid of images that best matches the query. These
+     * images will be combined into one big image (rastered) by the front end. <br>
+     *
+     *     The grid of images must obey the following properties, where image in the
+     *     grid is referred to as a "tile".
+     *     <ul>
+     *         <li>The tiles collected must cover the most longitudinal distance per pixel
+     *         (LonDPP) possible, while still covering less than or equal to the amount of
+     *         longitudinal distance per pixel in the query box for the user viewport size. </li>
+     *         <li>Contains all tiles that intersect the query bounding box that fulfill the
+     *         above condition.</li>
+     *         <li>The tiles must be arranged in-order to reconstruct the full image.</li>
+     *     </ul>
+     *
+     * @param request RasterRequest
+     * @return RasterResult
+     */
+    public static RasterResult rasterizeMap(RasterRequest request) {
+        RasterRequest r = request;
+        double top = r.ullat;
+        double bottom = r.lrlat;
+        double left = r.ullon;
+        double right = r.lrlon;
+
+        int depth = r.depth;
+
+        int leftY = (int) ((top - ROOT_ULLAT) / (-1 * LAT_PER_TILE[depth]));
+        int rightY = (int) ((bottom - ROOT_ULLAT) / (-1 * LAT_PER_TILE[depth]));
+        int leftX = (int) ((left - ROOT_ULLON) / LON_PER_TILE[depth]);
+        int rightX = (int) ((right - ROOT_ULLON) / LON_PER_TILE[depth]);
+
+        int height = Math.abs(leftY - rightY);
+        int width = Math.abs(leftX - rightX);
+
+        Tile[][] grid = new Tile[height+1][width+1];
+        for (int y = leftY; y <= rightY; y++) {
+            for (int x = leftX; x <= rightX; x++) {
+                Tile t = new Tile(depth, x, y);
+                grid[y - leftY][x - leftX] = t;
+            }
+        }
+        return new RasterResult(grid);
+
+    }
+
+    public static class Tile {
+        public final int depth;
+        public final int x;
+        public final int y;
+
+        public Tile(int depth, int x, int y) {
+            this.depth = depth;
+            this.x = x;
+            this.y = y;
+        }
+
+        public Tile offset() {
+            return new Tile(depth, x + 1, y + 1);
+        }
+
+        /**
+         * Return the latitude of the upper-left corner of the given slippy map tile.
+         * @return latitude of the upper-left corner
+         * @source https://wiki.openstreetmap.org/wiki/Slippy_map_tilenames
+         */
+        public double lat() {
+            double n = Math.pow(2.0, MIN_ZOOM_LEVEL + depth);
+            int slippyY = MIN_Y_TILE_AT_DEPTH[depth] + y;
+            double latRad = Math.atan(Math.sinh(Math.PI * (1 - 2 * slippyY / n)));
+            return Math.toDegrees(latRad);
+        }
+
+        /**
+         * Return the longitude of the upper-left corner of the given slippy map tile.
+         * @return longitude of the upper-left corner
+         * @source https://wiki.openstreetmap.org/wiki/Slippy_map_tilenames
+         */
+        public double lon() {
+            double n = Math.pow(2.0, MIN_ZOOM_LEVEL + depth);
+            int slippyX = MIN_X_TILE_AT_DEPTH[depth] + x;
+            return slippyX / n * 360.0 - 180.0;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
+            Tile tile = (Tile) o;
+            return depth == tile.depth &&
+                    x == tile.x &&
+                    y == tile.y;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(depth, x, y);
+        }
+
+        @Override
+        public String toString() {
+            return "d" + depth + "_x" + x + "_y" + y + ".jpg";
+        }
+    }
+}
